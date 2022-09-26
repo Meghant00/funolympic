6187b8d8035eaf28b5651db610c43c23c4735b93
Meghant00 <meghant@reconwithme.com>
2022-09-26 13:29:51 +0545

    Style: Change padding of blocked dashboard

--- src/components/dashboard/BlockedDashboard.vue
+++ src/components/dashboard/BlockedDashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full py-16 px-8">
+  <div class="w-full py-32 px-8">
     <n-alert title="User Blocked" type="error">
       You are currently Blocked by admin
     </n-alert>
